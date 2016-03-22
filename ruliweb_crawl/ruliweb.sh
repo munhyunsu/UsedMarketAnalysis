309d8d317f6149ba54a435f9e83feca19f3b5a3a
Mun Hyun Su <munhyunsu@gmail.com>
2016-03-22 14:49:26 +0900

    루리웹 NDS Database 사용하도록 변경

--- ruliweb_crawl/ruliweb.sh
+++ ruliweb_crawl/ruliweb.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-mkdir -p ./output/ps
-mkdir -p ./output_phone/ps
-scrapy crawl ps
-echo 'ps Done'
-sleep 5
-
-mkdir -p ./output/xbox
-mkdir -p ./output_phone/xbox
-scrapy crawl xbox
-echo 'xbox Done'
-sleep 5
-
-mkdir -p ./output/ngc
-mkdir -p ./output_phone/ngc
-scrapy crawl ngc
-echo 'ngc Done'
-sleep 5
-
+#mkdir -p ./output/ps
+#mkdir -p ./output_phone/ps
+#scrapy crawl ps
+#echo 'ps Done'
+#sleep 5
+#
+#mkdir -p ./output/xbox
+#mkdir -p ./output_phone/xbox
+#scrapy crawl xbox
+#echo 'xbox Done'
+#sleep 5
+#
+#mkdir -p ./output/ngc
+#mkdir -p ./output_phone/ngc
+#scrapy crawl ngc
+#echo 'ngc Done'
+#sleep 5
+#
 mkdir -p ./output/nds
-mkdir -p ./output_phone/nds
+#mkdir -p ./output_phone/nds
 scrapy crawl nds
 echo 'nds Done'
 sleep 5
-
-mkdir -p ./output/psp
-mkdir -p ./output_phone/psp
-scrapy crawl psp
-echo 'psp Done'
-sleep 5
+#
+#mkdir -p ./output/psp
+#mkdir -p ./output_phone/psp
+#scrapy crawl psp
+#echo 'psp Done'
+#sleep 5
