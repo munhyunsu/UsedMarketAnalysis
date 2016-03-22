6b7efec32eecf272702f1fe8212fab9753fff057
Mun Hyun Su <munhyunsu@gmail.com>
2016-03-23 02:00:20 +0900

    Make SQLite Shell File 변경

--- util_make_sqlite/make_sqlite.sh
+++ util_make_sqlite/make_sqlite.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo 'Joonggonara Start'
 python make_sqlite.py ../joonggonara_crawl/output/ joonggonara.sqlite
+echo 'Ppomppu Start'
 python make_sqlite.py ../ppomppu_crawl/output/ ppomppu.sqlite
+echo 'Ruliweb Start'
 python make_sqlite.py ../ruliweb_crawl/output/ ruliweb.sqlite
