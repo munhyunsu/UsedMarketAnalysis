e6c59813856fa4f612f876fe180bd77ad1887e14
Mun Hyun Su <munhyunsu@gmail.com>
2016-04-05 23:55:35 +0900

    Start Calculate IP Distance

---
+++ ruliweb_analyzer/ruliweb_ip_distance.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+
+##### ##### ===== 포함 지역 =====
+# 인자
+import sys
+# Database
+import sqlite3
+# Maxmind Database
+import geoip2.database
+# IP2Location Database
+import IP2Location
+##### ##### ===== 포함 지역 끝 =====
+
+
+
+
+
+##### ##### ===== 함수 선언 지역 =====
+
+
+
+
+
+##### ##### ===== 함수 선언 지역 끝 =====
+
+
+
+
+
+if __name__ == '__main__':
+	main()
